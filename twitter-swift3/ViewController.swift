f9cc3c094e57169d3ce515a5e55f1e236224f752
kmolo <kmoloney@mail.ccsf.edu>
2017-02-19 10:58:50 -0800

    Initial Commit

---
+++ twitter-swift3/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  twitter-swift3
+//
+//  Created by mny on 2/19/17.
+//  Copyright © 2017 ccsf. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
